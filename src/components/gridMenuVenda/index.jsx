2983367df9b49846e79e305c37cd4c4239f81c37
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2022-11-17 16:35:01 -0300

    forms

--- src/components/gridMenuVenda/index.jsx
+++ src/components/gridMenuVenda/index.jsx
@@ -9,15 +9,32 @@ function GridMenuVenda(props) {
         <div className="wrapper">
             <AsideMenu />
             <div>
+                <h4 className='layout_buttons'>Venda</h4>
                 <div className='layout_buttons'>
                     <Buttons name='Inclusão'/>
                     <Buttons name='Manutenção'/>
                     <Buttons name='A Receber'/>
                     <Buttons name='Relatório'/>
                 </div>
-                <div className='layout_forms'>
-                    <Form placeholder='Dt. Emissão'/>
-                    <Form placeholder='Senha'/>
+                <div className='row'>
+                    <div className='layout_forms'>
+                        <Form placeholder='Dt. Emissão'/>
+                        <Form placeholder='Senha'/>
+                        <Form placeholder='Código'/>
+                        <Form placeholder='Nome do Cliente'/>
+                        <Form placeholder='Vendedor'/>
+                    </div>
+                    <div className='layout_forms'>
+                        <Form placeholder='Código'/>
+                        <Form placeholder='Nome do Produto'/>
+                        <Form placeholder='Ult. Dt. Venda'/>
+                        <Form placeholder='Unidade'/>
+                        <Form placeholder='Peso'/>
+                        <Form placeholder='Estoque'/>
+                        <Form placeholder='Quantidade'/>
+                        <Form placeholder='Pr. Unitario'/>
+
+                    </div>
                 </div>
             </div>
             
