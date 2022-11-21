e575dacd072b59fe953696df1b0da8c0351622f8
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2022-11-21 14:49:59 -0300

    remove href

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg bg-light " >
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Rodinfo</a>
+                    <a className="navbar-brand" >Rodinfo</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
