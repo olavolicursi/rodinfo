ef4f23a8373d6b10c2af89d5c2099171bcaa964d
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2022-11-24 11:13:02 -0300

    href

--- src/components/navItemDropdown/index.jsx
+++ src/components/navItemDropdown/index.jsx
@@ -2,31 +2,31 @@ function NavItemDropdown(props) {
     return(
         <>
             <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">
                 {props.name}
             </a>
             <ul className="dropdown-menu">
-                <li><a className="dropdown-item">{props.item1}</a></li>
-                <li><a className="dropdown-item">{props.item2}</a></li>
-                <li><a className="dropdown-item">{props.item3}</a></li>
-                <li><a className="dropdown-item">{props.item4}</a></li>
-                <li><a className="dropdown-item">{props.item5}</a></li>
-                <li><a className="dropdown-item">{props.item6}</a></li>
-                <li><a className="dropdown-item">{props.item7}</a></li>
-                <li><a className="dropdown-item">{props.item8}</a></li>
-                <li><a className="dropdown-item">{props.item9}</a></li>
-                <li><a className="dropdown-item">{props.item10}</a></li>
-                <li><a className="dropdown-item">{props.item11}</a></li>
-                <li><a className="dropdown-item">{props.item12}</a></li>
-                <li><a className="dropdown-item">{props.item13}</a></li>
-                <li><a className="dropdown-item">{props.item14}</a></li>
-                <li><a className="dropdown-item">{props.item15}</a></li>
-                <li><a className="dropdown-item">{props.item16}</a></li>
-                <li><a className="dropdown-item">{props.item17}</a></li>
-                <li><a className="dropdown-item">{props.item18}</a></li>
-                <li><a className="dropdown-item">{props.item19}</a></li>
-                <li><a className="dropdown-item">{props.item20}</a></li>
-                <li><a className="dropdown-item">{props.item21}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item1}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item2}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item3}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item4}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item5}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item6}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item7}</a></li> 
+                <li><a className="dropdown-item" href="#">{props.item8}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item9}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item10}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item11}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item12}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item13}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item14}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item15}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item16}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item17}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item18}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item19}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item20}</a></li>
+                <li><a className="dropdown-item" href="#">{props.item21}</a></li>
             </ul>
             </li>
         </>
